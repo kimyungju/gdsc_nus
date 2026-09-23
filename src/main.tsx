a7a651ba6710995b1d08774ef4ed45952d1fdb53
Kim Yungju <yjkim101002@gmail.com>
2026-09-23 22:27:07 +0800

    Remove hash from site URLs

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+const legacyPath = window.location.hash.slice(1);
+if (legacyPath.startsWith("/") && URL.canParse(legacyPath, window.location.origin)) {
+  const legacyUrl = new URL(legacyPath, window.location.origin);
+
+  if (legacyUrl.origin === window.location.origin) {
+    new URLSearchParams(window.location.search).forEach((value, key) => {
+      legacyUrl.searchParams.append(key, value);
+    });
+    window.history.replaceState(null, "", legacyUrl.href);
+  }
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
